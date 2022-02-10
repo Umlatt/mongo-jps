8c9c650b0cb23eb0c91dd43d0832512395b03a26
Richard Raymond <richard.raymond@datasciences.co.za>
2022-02-10 12:58:38 +0200

    Added first steps

--- scripts/init-master.sh
+++ scripts/init-master.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo "Configuring master..." >> README.md
+echo "# Allow replication connections
+host     replication     repuser         0.0.0.0/0        trust" >> /var/lib/postgresql/data/pg_hba.conf
--- scripts/init-slave.sh
+++ scripts/init-slave.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo "Configuring slave..." >> README.md
+echo "# Allow replication connections
+host     replication     repuser         0.0.0.0/0        trust" >> /var/lib/postgresql/data/pg_hba.conf
