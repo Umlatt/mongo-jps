994728ef4c3c7f70a8de98211fb247159559d22a
Richard Raymond <richard.raymond@datasciences.co.za>
2022-02-11 09:47:54 +0200

    Added generic init

--- scripts/init-postgres-all.sh
+++ scripts/init-postgres-all.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "Configuring master..." >> README.md
 echo "# Allow replication connections
 host     replication     repuser         0.0.0.0/0        trust" >> /var/lib/postgresql/data/pg_hba.conf
--- scripts/init-slave.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Configuring slave..." >> README.md
-echo "# Allow replication connections
-host     replication     repuser         0.0.0.0/0        trust" >> /var/lib/postgresql/data/pg_hba.conf
