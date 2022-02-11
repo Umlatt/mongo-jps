8ee43f37d81a7c192b075b26d327dd385b574f2c
Richard Raymond <richard.raymond@datasciences.co.za>
2022-02-11 11:46:03 +0200

    Improved install log

--- scripts/init-postgres-master.sh
+++ scripts/init-postgres-master.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Step 2. Postgres - Init Master Node" >> README.md
+echo "Step 2 - Postgres - Init Master Node" >> README.md
--- scripts/init-postgres-slave.sh
+++ scripts/init-postgres-slave.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Step 3. Postgres - Init Slave Node" >> README.md
+echo "Step 3 - Postgres - Init Slave Node" >> README.md
