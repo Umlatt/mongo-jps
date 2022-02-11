3f370ed4f87d716761ac34e20db1a5b6a3b9129d
Richard Raymond <richard.raymond@datasciences.co.za>
2022-02-11 14:04:02 +0200

    Improved logging

--- scripts/init-postgres-master.sh
+++ scripts/init-postgres-master.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 password=$(cat replication_password)
-echo "Creating replication user..." >> INSTALL_LOG
+echo " Creating replication user." >> INSTALL_LOG
 # Create archive location for replication
 mkdir /var/lib/postgresql/archive
 chown postgres:postgres /var/lib/postgresql/archive
 # PostgreSQL - Create the replication user
-#gosu postgres postgres --single <<-EOSQL
-#   CREATE ROLE repuser PASSWORD 'md5${1234}' REPLICATION LOGIN;
-#EOSQL
-echo "Using password: $password"
+echo " Using password: $password" >> INSTALL_LOG
 sudo -u postgres -H -- psql -c "CREATE ROLE repuser PASSWORD 'md5${password}' REPLICATION LOGIN;"
--- scripts/init-postgres-slave.sh
+++ scripts/init-postgres-slave.sh
@@ -1,2 +1,1 @@
 #!/bin/bash
-echo "Step 3 - Postgres - Init Slave Node" >> README.md
