c8401e151bf2b4531be86354bc039d6c38ce1bd5
Richard Raymond <theplacetosendmemail@gmail.com>
2022-02-14 13:23:43 +0200

    Added pip to log

--- scripts/init-postgres-all.sh
+++ scripts/init-postgres-all.sh
@@ -2,9 +2,9 @@
 
 PGSQL_DATA='/var/lib/postgresql/data'
 # Create archive location for replication
-echo " Creating archive space"
+echo " Creating archive space" >> INSTALL_LOG
 mkdir /var/lib/postgresql/archive
 chown postgres:postgres /var/lib/postgresql/archive
 # Create replication user access
-echo -e "\n# Allow replication connections for remote DBs
+echo -e "\n\n# Allow replication connections for remote DBs
 host     replication     repuser         0.0.0.0/0          md5" >> $PGSQL_DATA/pg_hba.conf
