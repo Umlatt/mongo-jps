6fb0d089c66413de92f0afb715e7be8df22453b9
Richard Raymond <theplacetosendmemail@gmail.com>
2022-02-14 13:08:13 +0200

    Added generic postgres settings

--- scripts/init-postgres-all.sh
+++ scripts/init-postgres-all.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+PGSQL_DATA='/var/lib/postgresql/data'
+# Create archive location for replication
+echo " Creating archive space"
+mkdir /var/lib/postgresql/archive
+chown postgres:postgres /var/lib/postgresql/archive
+# Create replication user access
 echo -e "\n# Allow replication connections for remote DBs
-host     replication     repuser         0.0.0.0/0          md5" >> /var/lib/postgresql/data/pg_hba.conf
+host     replication     repuser         0.0.0.0/0          md5" >> $PGSQL_DATA/pg_hba.conf
