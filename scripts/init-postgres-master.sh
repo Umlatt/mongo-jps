01d7450dcacf65725843cba4f77e4bab357482f1
Richard Raymond <theplacetosendmemail@gmail.com>
2022-02-14 14:40:12 +0200

    https://girders.org/postgresql/2021/11/05/setup-postgresql14-replication/

--- scripts/init-postgres-master.sh
+++ scripts/init-postgres-master.sh
@@ -6,3 +6,6 @@ PGSQL_DATA='/var/lib/postgresql/data'
 echo " Creating replication user" >> INSTALL_LOG
 echo " Using password: $reppassword" >> INSTALL_LOG
 sudo -u postgres -H -- psql -c "CREATE ROLE repuser PASSWORD 'md5${reppassword}' REPLICATION LOGIN;"
+
+sudo -u postgres -H -- psql -c "SELECT * from pg_create_physical_replication_slot('repl_slot');"
+sudo -u postgres -H -- psql -c "SELECT slot_name, slot_type, active, wal_status FROM pg_replication_slots;" >> INSTALL_LOG
--- scripts/init-postgres-slave.sh
+++ scripts/init-postgres-slave.sh
@@ -8,22 +8,31 @@ echo " Stopping DB" >> INSTALL_LOG
 sudo systemctl stop postgresql
 # Clear current data directory (excluding posgresql.conf & pg_hba.conf)
 echo " Cleaning old data directory [$PGSQL_DATA]" >> INSTALL_LOG
-sudo cp $PGSQL_DATA/postgresql.conf /tmp/.
-sudo cp $PGSQL_DATA/pg_hba.conf /tmp/.
+#sudo cp $PGSQL_DATA/postgresql.conf /tmp/.
+#sudo cp $PGSQL_DATA/pg_hba.conf /tmp/.
 sudo rm -rf $PGSQL_DATA/*
-sudo mv /tmp/postgresql.conf $PGSQL_DATA/.
-sudo mv /tmp_pg_hba.conf $PGSQL_DATA/.
+#sudo mv /tmp/postgresql.conf $PGSQL_DATA/.
+#sudo mv /tmp_pg_hba.conf $PGSQL_DATA/.
+echo " Sync data from master db." >> INSTALL_LOG
+sudo -u postgres pg_basebackup --pgdata $PGSQL_DATA \
+    --format=p --write-recovery-conf --checkpoint=fast \
+    --label=mffb --progress --host=$masterip --port=5432 \
+    --username=repuser --password=$reppassword
 
 # Configure node to host_standby node
-echo " Adding slave config to [$PGSQL_DATA/postgresql.conf]" >> INSTALL_LOG
-sudo echo "hot_standby = on" >> $PGSQL_DATA/postgresql.conf
-sudo echo "primary_conninfo = 'host=${masterip} port=5432 user=repuser password=${reppassword}'" >> ${PGSQL_DATA}/postgresql.conf;
-sudo echo "promote_trigger_file = '/tmp/postgresql.trigger.5432'" >> ${PGSQL_DATA}/postgresql.conf;
+#echo " Adding slave config to [$PGSQL_DATA/postgresql.conf]" >> INSTALL_LOG
+#sudo echo "hot_standby = on" >> $PGSQL_DATA/postgresql.conf
+#sudo echo "primary_conninfo = 'host=${masterip} port=5432 user=repuser password=${reppassword}'" >> ${PGSQL_DATA}/postgresql.conf;
+#sudo echo "promote_trigger_file = '/tmp/postgresql.trigger.5432'" >> ${PGSQL_DATA}/postgresql.conf;
 # Add standby signal file
-echo " Adding standby.signal file." >> INSTALL_LOG
-sudo touch $PGSQL_DATA/standby.signal 
+#echo " Adding standby.signal file." >> INSTALL_LOG
+#sudo touch $PGSQL_DATA/standby.signal 
+
+echo " Add replication settings to postgresql conf" >> INSTALL_LOG
+echo "\n\n# Standby" >> $PGSQL_DATA/postgresql.conf
+echo "primary_conninfo = 'user=repuser port=5432 host=$masterip application_name=replica'">> $PGSQL_DATA/postgresql.conf
+echo "primary_slot_name = 'repl_slot'" >> $PGSQL_DATA/postgresql.conf
 
 # Start database services
 echo " Starting DB" >> INSTALL_LOG
 sudo systemctl start postgresql
-
