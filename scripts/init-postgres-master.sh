200069eb172b423dc8094d8bc0781e88744113c1
Richard Raymond <theplacetosendmemail@gmail.com>
2022-02-14 12:31:27 +0200

    BASH to SH Rework

--- scripts/init-postgres-master.sh
+++ scripts/init-postgres-master.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-password=$(cat replication_password)
+reppassword=$1
 echo " Creating replication user." >> INSTALL_LOG
 # Create archive location for replication
 mkdir /var/lib/postgresql/archive
 chown postgres:postgres /var/lib/postgresql/archive
 # PostgreSQL - Create the replication user
-echo " Using password: $password" >> INSTALL_LOG
-sudo -u postgres -H -- psql -c "CREATE ROLE repuser PASSWORD 'md5${password}' REPLICATION LOGIN;"
+echo " Using password: $reppassword" >> INSTALL_LOG
+sudo -u postgres -H -- psql -c "CREATE ROLE repuser PASSWORD 'md5${reppassword}' REPLICATION LOGIN;"
--- scripts/init-postgres-slave.sh
+++ scripts/init-postgres-slave.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+reppassword=$1
+masterip=$2
+echo "$reppassword [$masterip]" > TEMP.txt
