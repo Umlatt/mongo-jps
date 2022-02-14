486044b7b2b2bf79306413932870847d2b57a20d
Richard Raymond <theplacetosendmemail@gmail.com>
2022-02-14 16:44:50 +0200

    Removed possbile stupid characters

--- scripts/init-postgres-all.sh
+++ scripts/init-postgres-all.sh
@@ -6,5 +6,5 @@ echo " Creating archive space" >> INSTALL_LOG
 mkdir /var/lib/postgresql/archive
 chown postgres:postgres /var/lib/postgresql/archive
 # Create replication user access
-echo -e "\n\n# Allow replication connections for remote DBs
+echo "# Allow replication connections for remote DBs
 host     replication     repuser         0.0.0.0/0          md5" >> $PGSQL_DATA/pg_hba.conf
