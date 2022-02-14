ba2189085dbb48b0723060414437d980593d6f79
Richard Raymond <theplacetosendmemail@gmail.com>
2022-02-14 13:07:37 +0200

    Added entire config to file

--- scripts/init-postgres-slave.sh
+++ scripts/init-postgres-slave.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 reppassword=$1
 masterip=$2
-echo "$reppassword [$masterip]" > TEMP.txt
+PGSQL_DATA='/var/lib/postgresql/data'
+
+# Stop database services
+sudo /etc/init.d/postgresql stop
+# Clear current data directory (excluding posgresql.conf & pg_hba.conf)
+rm -rf !($PGSQL_DATA/postgresql.conf|$PGSQL_DATA/pg_hba.conf)
+
+# Configure node to host_standby node
+echo "hot_standby = on" >> $PGSQL_DATA/postgresql.conf
+echo "primary_conninfo = 'host=${masterip} port=5432 user=repuser password=${reppassword}'" >> ${PGSQL_DATA}/postgresql.conf;
+echo "promote_trigger_file = '/tmp/postgresql.trigger.5432'" >> ${PGSQL_DATA}/postgresql.conf;
+
+# Add standby signal file
+touch /var/lib/postgresql/data/standby.signal 
+
+# Start database services
+sudo /etc/init.d/postgresql start
+
