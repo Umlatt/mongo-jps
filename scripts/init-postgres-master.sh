756c40c569320db6475cc4c2e581ccf5c1d53645
Richard Raymond <theplacetosendmemail@gmail.com>
2022-02-14 13:08:35 +0200

    Moved generic settings to postgres-all

--- scripts/init-postgres-master.sh
+++ scripts/init-postgres-master.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-reppassword=$1
-echo " Creating replication user." >> INSTALL_LOG
-# Create archive location for replication
-mkdir /var/lib/postgresql/archive
-chown postgres:postgres /var/lib/postgresql/archive
+reppassword=$
+PGSQL_DATA='/var/lib/postgresql/data'
+
 # PostgreSQL - Create the replication user
+echo " Creating replication user" >> INSTALL_LOG
 echo " Using password: $reppassword" >> INSTALL_LOG
 sudo -u postgres -H -- psql -c "CREATE ROLE repuser PASSWORD 'md5${reppassword}' REPLICATION LOGIN;"
