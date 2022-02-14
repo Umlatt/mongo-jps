27309737eac2562206e048189dac65d4bac525ea
Richard Raymond <theplacetosendmemail@gmail.com>
2022-02-14 21:00:47 +0200

    Added reload of user config

--- scripts/init-postgres-master.sh
+++ scripts/init-postgres-master.sh
@@ -4,8 +4,13 @@ PGSQL_DATA='/var/lib/postgresql/data'
 
 # PostgreSQL - Create the replication user
 echo " Creating replication user" >> INSTALL_LOG
+sudo -u postgres -H -- psql -c "SELECT pg_reload_conf();"
 echo " Using password: $reppassword" >> INSTALL_LOG
 sudo -u postgres -H -- psql -c "CREATE ROLE repuser PASSWORD 'md5${reppassword}' REPLICATION LOGIN;"
 
 sudo -u postgres -H -- psql -c "SELECT * from pg_create_physical_replication_slot('repl_slot');"
 sudo -u postgres -H -- psql -c "SELECT slot_name, slot_type, active, wal_status FROM pg_replication_slots;" >> INSTALL_LOG
+
+# sudo -u postgres -H -- psql -c "SELECT pg_reload_conf();" # Reload user permissions
+# sudo -u postgres -H -- psql -c "select * from pg_hba_file_rules;"
+# sudo -u postgres -H -- psql -c "\du;"
