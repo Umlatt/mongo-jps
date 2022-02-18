af3d0a735358e1218a49e8d435fe5cfbce2c98d2
Richard Raymond <richard.raymond@datasciences.co.za>
2022-02-18 11:25:07 +0200

    Initial upload

---
+++ scripts/check_master.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ipaddress=$1
+PGSQL_DATA='/var/lib/postgresql/data'
+
+while nc -z $ipaddress 5432; do
+  echo -n "." >> REPLICATION_STATUS
+  sleep 1 # wait for 1/10 of the second before check again
+done
+echo "\n\nMaster DB is unavailable. Promoting this slave to replica.\n" >> REPLICATION_STATUS
+sudo -u postgres pg_ctl promote -D $PGSQL_DATA >> REPLICATION_STATUS
